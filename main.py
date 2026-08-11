8730c4b19d0ffec9f123518edb48ab11348ca3f0
dibyendukhatua1973-ship-it <dibyendukhatua1973@gmail.com>
2026-08-11 19:59:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-BOT_TOKEN = "8974579957:AAgoqSED-miv5Vi5NGE6kDENYa6v3QE214K"
+BOT_TOKEN = "8974579957:AAGoqSEd-miv5Vi5NGE6kdENya6v3QE214k"
 
 client = genai.Client(api_key="AQ.Ab8RN6LJsYPtGEZbLf2RFe04dj9CcQXQFuiUNba7LRtNXJ51QhQ")
 model_name = 'gemini-1.5-flash'
